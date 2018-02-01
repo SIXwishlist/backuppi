a4a3ecddbe36e8ec5592d8aa3442f3732b1cc54f
Davide Aversa <thek3nger@gmail.com>
2018-02-01 19:21:07 +0100

    Another step in the scan direction

--- backuppid.sh
+++ backuppid.sh
@@ -8,8 +8,13 @@ BKP_DIR=$BACKUPPI_BKP_DIR
 
 echo "Scan $BKP_DIR"
 
-for host in $(ls -d $BKP_DIR/*)
+for host in $(find $BKP_DIR -maxdepth 1 -type d)
 do
-    echo $host
+    echo "HOST" $host
+    for dest in $(find $host -maxdepth 1 -type d)
+    do
+        echo "SUB FOLDER" $dest
+        echo rsync –av --delete $dest/daily $dest/weekly
+    done
 done
 
