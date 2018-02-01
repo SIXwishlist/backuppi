fd281cfda28f1edac76a4176ef4d6f0d08ed8320
Davide Aversa <thek3nger@gmail.com>
2018-02-01 19:02:43 +0100

    Scan backup folder

--- backuppid.sh
+++ backuppid.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # Server demon application for Backuppi
 #
-# It automatically scann the backup folder performing weekly and monthly
+# It automatically scan the backup folder performing weekly and monthly
 # snapshots. It also upload the monthly snapshot online (if needed)
+
+BKP_DIR=$BACKUPPI_BKP_DIR
+
+echo "Scan $BKP_DIR"
+
+for host in $(ls -d $BKP_DIR/*)
+do
+    echo $host
+done
+
